99d0531c737d5fe932cafb235bf84ec23591b630
Hull-Coleen <ch.hull@verizon.net>
2020-04-21 17:22:09 -0400

    Week01 assignment start

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Coleen Hull";
+            string location = "Sterling Va";
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I live in  " + location);
+        }
+    }
+}
