da2b5dce8033dd80fe5f9b950592fabd172980ac
Hull-Coleen <ch.hull@verizon.net>
2020-04-21 22:00:38 -0400

    Added date and days til Christmas

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -8,8 +8,20 @@ namespace ConsoleApplication
         {
             string name = "Coleen Hull";
             string location = "Sterling Va";
-            Console.WriteLine($"My name is {name}");
-            Console.WriteLine($"I live in {location}");
+            string date = DateTime.UtcNow.ToString("d");
+            DateTime Christmas = new DateTime(2020, 12, 25);
+            double daysUntilChristmas = Christmas.Subtract(DateTime.Today).TotalDays;
+            Console.WriteLine($"My name is {name}.");
+            Console.WriteLine($"I live in {location}.");
+            Console.WriteLine("The date is {0}", date);
+            Console.WriteLine($"There are {daysUntilChristmas}  days until Christmas");
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I live in " + location);
+            Console.WriteLine("The date is {0}", date);
+            Console.WriteLine("There are " + daysUntilChristmas +
+                " days until Christmas");
+
+
         }
     }
 }
