12c6cff67b611c1e69e666193e92d93d18b4b678
Hull-Coleen <ch.hull@verizon.net>
2020-06-11 09:05:39 -0400

    added images

--- MegaDesk/Models/DeskQuote.cs
+++ MegaDesk/Models/DeskQuote.cs
@@ -12,12 +12,21 @@ namespace MegaDesk.Models
         public int ID { get; set; }
         [DataType(DataType.Date)]
         public DateTime QuoteDate { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string FirstName { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string LastName { get; set; }
+        [Range(24, 96)]
+        [Required]
         public int Width { get; set; }
+        [Range(12, 48)]
+        [Required]
         public int Depth { get; set; }
         public int DrawerNumber { get; set; }
         public string Material { get; set; }
+        [DataType(DataType.Currency)]
         public decimal Cost { get; set; }
         public string Shipping { get; set; }
 
