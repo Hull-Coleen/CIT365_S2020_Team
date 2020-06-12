60fc6a07089b41118eb7874675e3d78d6572075d
cuellaryamilka <ycp12288@gmail.com>
2020-06-11 22:11:18 -0400

    trying to make the images to show by slecting materials 06-11-20

---
+++ MegaDesk/wwwroot/js/Create.js
@@ -0,0 +1,27 @@
+﻿
+// 
+var select = document.querySelector('select');
+var html = document.querySelector('materialView');
+
+// create even listener
+select.addEventListener('change', backgrounds);
+
+function backgrounds() {
+    var choice = select.value;
+
+    if (choice === "Veneer") {
+        html.style.backgroundImage = "url('images/venner.jpg')";
+    }
+    else if (choice === "Oak") {
+        html.style.backgroundImage = "url('images/oak.jpg')";
+    }
+    else if (choice === "Laminate") {
+        html.style.backgroundImage = "url('images/laminate.jpg')";
+    }
+    else if (choice === "Pine") {
+        html.style.backgroundImage = "url('images/pinejpg.jpg')";
+    }
+    else if (choice === "Rosewood") {
+        html.style.backgroundImage = "url('images/Rosewood.jpg')";
+    }
+}
