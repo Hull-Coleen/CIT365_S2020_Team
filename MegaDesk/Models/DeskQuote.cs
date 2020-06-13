1a572d2146cdb30e55724d9cd8d9e2e4c3666dc7
Hull-Coleen <ch.hull@verizon.net>
2020-06-13 08:12:17 -0400

    made things more uniform had input match size

--- MegaDesk/Models/DeskQuote.cs
+++ MegaDesk/Models/DeskQuote.cs
@@ -36,49 +36,5 @@ namespace MegaDesk.Models
         [Display(Name = "Ship Time")]
         public string Shipping { get; set; }
 
-        public double cost()
-        {
-            int size = Width * Depth;
-            if (size > 1000)
-            {
-                size = Width * Depth - 1000;
-            }
-            else 
-                size = 0;
-            int MaterialCost = 0;
-            if (Material == "Pine")
-                MaterialCost = 50;
-            else if (Material == "Oak")
-                MaterialCost = 100;
-            else if (Material == "Veneer")
-                MaterialCost = 150;
-            else if (Material == "Laminate")
-                MaterialCost = 175;
-            else if(Material == "Rosewood")
-                    MaterialCost = 300;
-            return 200.00 + (DrawerNumber * 50) + size + MaterialCost;
-        }
-        public double cost(string material, int width, int depth, int drawer)
-        {
-            int size = width * depth;
-            if (size > 1000)
-            {
-                size = width * depth - 1000;
-            }
-            else
-                size = 0;
-            int MaterialCost = 0;
-            if (material == "Pine")
-                MaterialCost = 50;
-            else if (material == "Oak")
-                MaterialCost = 100;
-            else if (material == "Veneer")
-                MaterialCost = 150;
-            else if (material == "Laminate")
-                MaterialCost = 175;
-            else if (material == "Rosewood")
-                MaterialCost = 300;
-            return 200.00 + (drawer * 50) + size + MaterialCost;
-        }
     }
 }
