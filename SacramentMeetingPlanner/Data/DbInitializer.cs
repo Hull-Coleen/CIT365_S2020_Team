abba545b912846f6c16cf6780dd64738ee8ebc9a
cuellaryamilka <ycp12288@gmail.com>
2020-07-14 14:07:00 -0400

    Added DbInitializer to seed DataBase

---
+++ SacramentMeetingPlanner/Data/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using SacramentMeetingPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SacramentContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any member
+            if (context.Members.Any())
+            {
+                return; // Db has been seeded
+            }
+
+            var members = new Member[]
+            {
+            new Member{FirstName="Pilar",LastName="Pedrozo",Calling="Relief Society President"},
+            new Member{FirstName="Pablo",LastName="Almirante",Calling="Elders Quorum President"},
+            new Member{FirstName="Lazaro",LastName="Valdez",Calling="Bishop"},
+            new Member{FirstName="Elena",LastName="Mendoza",Calling="Ward Music Chairman"},
+            new Member{FirstName="Sulivan",LastName="Mendez",Calling="Ward Music Director"},
+            new Member{FirstName="Claudia",LastName="Velasquez",Calling="Ward Organist or Pianist"},
+            new Member{FirstName="Diego",LastName="Montenegro",Calling="Elders Quorum Teacher"},
+            new Member{FirstName="Amanda",LastName="Rocoso",Calling="Ward Relief Society Teacher"},
+            };
+
+            foreach (Member s in members)
+                {
+                context.Members.Add(s);
+            }
+            context.SaveChanges();
+
+            
+
+        }
+        }
+    }
+
--- SacramentMeetingPlanner/Startup.cs
+++ SacramentMeetingPlanner/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SacramentMeetingPlanner.Data;
 
 namespace SacramentMeetingPlanner
 {
@@ -30,6 +32,9 @@ namespace SacramentMeetingPlanner
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<SacramentContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
