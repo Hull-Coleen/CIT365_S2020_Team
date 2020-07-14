7a5a82f649492b3d8df267e4e94fa466fe72eb39
cuellaryamilka <ycp12288@gmail.com>
2020-07-14 12:58:52 -0400

    Adding a Data folder with a SacramentContext class

---
+++ SacramentMeetingPlanner/Data/SacramentContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SacramentMeetingPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Data
+{
+    public class SacramentContext:DbContext
+    {
+        public SacramentContext(DbContextOptions<SacramentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SacramentMeeting> SacramentM { get; set; }
+        public DbSet<Hymn> Hymns { get; set; }
+        public DbSet<Member> Members { get; set; }
+    }
+}
